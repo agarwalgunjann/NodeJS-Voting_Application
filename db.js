8fc104dcee16b456c1a9981b7e75193221e92c4e
agarwalgunjann <agarwalgunjan2306@gmail.com>
2024-06-10 17:43:47 +0530

    Adding Inital files

---
+++ db.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+// Define the MongoDB connection URL
+const { MONGODB_URI } = process.env;
+
+// Set up MongoDB connection
+mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+// Get the default connection
+// Mongoose maintains a default connection object representing the MongoDB connection.
+const db = mongoose.connection;
+
+// Define event listeners for database connection
+
+db.on('connected', () => {
+    console.log('Connected to MongoDB server');
+});
+
+db.on('error', (err) => {
+    console.error('MongoDB connection error:', err);
+});
+
+db.on('disconnected', () => {
+    console.log('MongoDB disconnected');
+});
+
+// Export the database connection
+module.exports = db;
